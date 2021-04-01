6e8c6f67566920c0746bcd974234c12f2dfa8244
adba98 <oscar.adba@gmail.com>
2021-03-31 22:16:47 -0500

    Agregar index para cargar los modulos del modelo

---
+++ models/index.models.js
@@ -0,0 +1,11 @@
+const Categoria = require('./categoria');
+const Role = require('./rol');
+const Server = require('./server');
+const Usuario = require('./usuario');
+
+module.exports = {
+  Server,
+  Categoria,
+  Role,
+  Usuario
+}
