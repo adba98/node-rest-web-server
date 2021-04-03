43e4cec234bb3a794e99890880b0ccb181247acf
adba98 <oscar.adba@gmail.com>
2021-04-03 18:10:21 -0500

    Ruta para ontener la imagen del usuario o producto

--- controllers/uploads.controller.js
+++ controllers/uploads.controller.js
@@ -62,8 +62,53 @@ const actualizarImagen = async (req = request, res = response) => {
   }
 }
 
+const mostrarImagen = async (req = request, res = response) => {
+  const { id, coleccion } = req.params;
+  try {
+    let modelo;
+    switch (coleccion) {
+      case 'usuarios':
+        modelo = await Usuario.findById(id);
+        if (!modelo) {
+          return res.status(400).json({
+            msg: `No existe el usauario con id: ${id}`
+          });
+        }
+        break;
+      case 'productos':
+        modelo = await Producto.findById(id);
+        if (!modelo) {
+          return res.status(400).json({
+            msg: `No existe el producto con id: ${id}`
+          });
+        }
+        break;
+      default:
+        res.status(500).json({
+          msg: 'No se agrego la coleccion al actualizar'
+        });
+        break;
+    }
+
+    // Revisar y limpiar img previas
+    if (modelo.img) {
+      const pathImg = path.join(__dirname, '../uploads/', coleccion, modelo.img);
+      if (fs.existsSync(pathImg)) {
+        return res.sendFile(pathImg);
+      }
+    }
+
+    const pathImg = path.join(__dirname, '../assets/no-image.jpg');
+    return res.sendFile(pathImg);
+
+  } catch (error) {
+    res.status(400).json({ msg: `${error}` });
+  }
+}
+
 
 module.exports = {
   cargarArchivo,
-  actualizarImagen
+  actualizarImagen,
+  mostrarImagen
 }
--- routes/uploads.routes.js
+++ routes/uploads.routes.js
@@ -1,6 +1,10 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads.controller');
+const {
+  cargarArchivo,
+  actualizarImagen,
+  mostrarImagen
+} = require('../controllers/uploads.controller');
 const { coleccionesPermitidas } = require('../helpers');
 const { validarCampos, validarArchivo } = require('../middlewares');
 const router = Router();
@@ -13,5 +17,11 @@ router.put('/:coleccion/:id', [
   check('coleccion').custom((c) => coleccionesPermitidas(c, ['usuarios', 'productos'])),
   validarCampos
 ], actualizarImagen);
+
+router.get('/:coleccion/:id', [
+  check('id', 'El id debe ser de mongo').isMongoId(),
+  check('coleccion').custom((c) => coleccionesPermitidas(c, ['usuarios', 'productos'])),
+  validarCampos
+], mostrarImagen);
 
 module.exports = router;
